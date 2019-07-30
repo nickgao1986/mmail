edfbaa67246052299403a6a83d560c32971029cf
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-07-30 14:15:30 +0800

    fix bug

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -4,7 +4,6 @@ import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
-import com.sun.tools.javac.util.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
