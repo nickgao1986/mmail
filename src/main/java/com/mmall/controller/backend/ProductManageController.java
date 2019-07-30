7d796d4e7699c2305eea906170fc03187c59f7fe
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-07-30 12:07:10 +0800

    fix bug

--- src/main/java/com/mmall/controller/backend/ProductManageController.java
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Maps;
 import com.mmall.common.Const;
 import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
 import com.mmall.pojo.User;
 import com.mmall.service.IFileService;
 import com.mmall.service.IProductService;
@@ -36,6 +37,55 @@ public class ProductManageController {
 
     @Autowired
     private IFileService iFileService;
+
+    @RequestMapping("save.do")
+    @ResponseBody
+    public ServerResponse productSave(HttpSession session, Product product){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //填充我们增加产品的业务逻辑
+            return iProductService.saveOrUpdateProduct(product);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("set_sale_status.do")
+    @ResponseBody
+    public ServerResponse setSaleStatus(HttpSession session, Integer productId,Integer status){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            return iProductService.setSaleStatus(productId,status);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("detail.do")
+    @ResponseBody
+    public ServerResponse getDetail(HttpSession session, Integer productId){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //填充业务
+            return iProductService.manageProductDetail(productId);
+
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
 
     @RequestMapping("list.do")
     @ResponseBody
--- src/main/java/com/mmall/service/IProductService.java
+++ src/main/java/com/mmall/service/IProductService.java
@@ -2,11 +2,21 @@ package com.mmall.service;
 
 import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+import com.mmall.vo.ProductDetailVo;
 
 /**
  * Created by gaoyoujian on 2019/7/28.
  */
 public interface IProductService {
+
+
+    ServerResponse saveOrUpdateProduct(Product product);
+
+    ServerResponse<String> setSaleStatus(Integer productId,Integer status);
+
+    ServerResponse<ProductDetailVo> manageProductDetail(Integer productId);
+
 
     ServerResponse<PageInfo> getProductList(int pageNum, int pageSize);
 
