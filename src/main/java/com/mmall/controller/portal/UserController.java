4aaab31f954e56706d5c836dc7d0faf3b051bb76
gaoyoujian <gaoyoujian@babytree-inc.com>
2019-08-09 18:33:53 +0800

    fix bug

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,9 +1,12 @@
 package com.mmall.controller.portal;
 
 import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,6 +24,7 @@ import javax.servlet.http.HttpSession;
 @CrossOrigin
 public class UserController {
 
+    private static  final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
     @Autowired
     private IUserService iUserService;
@@ -36,24 +40,35 @@ public class UserController {
     @RequestMapping(value = "login.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session){
-        System.out.println("<<<<<login do");
+
         ServerResponse<User> response = iUserService.login(username,password);
         if(response.isSuccess()){
+            logger.info("<<<<<succes session="+session);
             session.setAttribute(Const.CURRENT_USER,response.getData());
         }
-//        response.addHeader("Access-Control-Allow-Origin","*");
-//        response.addHeader("Access-Control-Allow-Methods","*");
-//        response.addHeader("Access-Control-Max-Age","100");
-//        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-//        response.addHeader("Access-Control-Allow-Credentials","false");
 
+        logger.info("<<<<<session="+session);
         return response;
+    }
+
+    @RequestMapping(value = "logout.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
     }
 
     @RequestMapping(value = "register.do",method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<String> register(User user){
         return iUserService.register(user);
+    }
+
+
+    @RequestMapping(value = "check_valid.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> checkValid(String str,String type){
+        return iUserService.checkValid(str,type);
     }
 
 
@@ -66,6 +81,7 @@ public class UserController {
         }
         return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
     }
+
 
     @RequestMapping(value = "forget_get_question.do",method = RequestMethod.POST)
     @ResponseBody
@@ -89,5 +105,43 @@ public class UserController {
 
 
 
+    @RequestMapping(value = "reset_password.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> resetPassword(HttpSession session,String passwordOld,String passwordNew){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        return iUserService.resetPassword(passwordOld,passwordNew,user);
+    }
+
+
+    @RequestMapping(value = "update_information.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> update_information(HttpSession session,User user){
+       User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        logger.info("<<<<<currentUser1="+currentUser);
+        user.setId(currentUser.getId());
+        user.setUsername(currentUser.getUsername());
+        ServerResponse<User> response = iUserService.updateInformation(user);
+        if(response.isSuccess()){
+            response.getData().setUsername(currentUser.getUsername());
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    @RequestMapping(value = "get_information.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> get_information(HttpSession session){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登录,需要强制登录status=10");
+        }
+        return iUserService.getInformation(1);
+    }
 
 }
